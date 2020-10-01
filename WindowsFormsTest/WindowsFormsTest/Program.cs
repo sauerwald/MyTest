d4bac9a58e0e834f0b29abcad808a3c385942fa5
t.sauerwald <t.sauerwald@P-8S-0A101-0014>
2020-10-01 09:34:54 +0200

    git test

--- WindowsFormsTest/WindowsFormsTest/Program.cs
+++ WindowsFormsTest/WindowsFormsTest/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsTest
         [STAThread]
         static void Main()
         {
+            int test = 42;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
